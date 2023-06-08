4fec0dc1bf1bd3e1e5947deb58c08f0d2a5b4751
Mihail Mihaylov <tielass@gmail.com>
2023-06-09 02:31:56 +0300

    homepage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,21 @@
   import InstagramIcon from '@mui/icons-material/Instagram';
   import LinkedInIcon from '@mui/icons-material/LinkedIn';
   import MailOutlineIcon from '@mui/icons-material/MailOutline';
+  import '../style/Footer.css'
 
   function footer() {
     return (
       <div className='footer'>
         <p>What we do in life echoes in eternity. </p>
         <div className="social--media">
-        <Link><GitHubIcon></GitHubIcon></Link>
-        <Link><InstagramIcon></InstagramIcon></Link>
-        <Link><LinkedInIcon></LinkedInIcon></Link>
-        <Link><GitHubIcon></GitHubIcon></Link>
-        <Link><MailOutlineIcon></MailOutlineIcon></Link>
+          <Link><GitHubIcon></GitHubIcon></Link>
+          <Link><InstagramIcon></InstagramIcon></Link>
+          <Link><LinkedInIcon></LinkedInIcon></Link>
+          <Link><GitHubIcon></GitHubIcon></Link>
+          <Link><MailOutlineIcon></MailOutlineIcon></Link>
+        </div>
+        <div className="copyright">
+        <p>Mihail Mihaylov &copy; 2023</p>
         </div>
       </div>
     )
--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import IntroImg from '../images/intro-img.png'
+import '../style/Introduction.css'
 
 function Introduction() {
   return (
-    <div>
-      Introduction
+    <div className='intro--container'>
+      <div className="intro--content">
+        <div className="intro--info">
+          <h2>Mihail <br></br> Mihaylov</h2>
+          <p>I'm a Front-End Developer for  in Los Angeles, CA.
+          I have serious passion for UI effects, animations and creating intuitive, dynamic user experiences.
+          Let's make something special.</p>
+          <div className="social--media--intro">
+            <Link><GitHubIcon></GitHubIcon></Link>
+            <Link><InstagramIcon></InstagramIcon></Link>
+            <Link><LinkedInIcon></LinkedInIcon></Link>
+            <Link><GitHubIcon></GitHubIcon></Link>
+          </div>
+        </div>
+        <div className="intro--img--container">
+        <img src={ IntroImg } alt="introimage" className='intro--img' />
+        </div>
+      </div>
     </div>
   )
 }
