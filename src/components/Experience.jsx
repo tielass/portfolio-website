f114396317cef8a28f491a855d027025669c269b
Mihail Mihaylov <tielass@gmail.com>
2023-06-16 00:57:38 +0300

    timeline styling

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import '../style/Experience.css'
+import {experienceDb} from '../helpers/experienceDb'
+import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
+import LaptopMacOutlinedIcon from '@mui/icons-material/LaptopMacOutlined';
+import LocalLibraryOutlinedIcon from '@mui/icons-material/LocalLibraryOutlined';
 
 function Experience() {
   return (
@@ -9,18 +13,45 @@ function Experience() {
       <h2 className='experience--header'>Education & Experience</h2>
       <div className="experience--container">
       <VerticalTimeline>
-        <VerticalTimelineElement>
-          <h1>first element</h1>
+        {/* <VerticalTimelineElement icon={<SchoolOutlinedIcon className='timeline--icon'/>} >
+          <h2>GRAPHIC DESIGNER - 11. 2018 - 09. 2022</h2>
+          <p className='experience-city'>Berlin</p>
+          <p>LemonOne GmbH</p>
+          <p>Supported the development of the start-up with visuals for internal use, such as guidelines, logos and printable materials, as well as external such as client guidelines. Managed a team of more than 20 external partners and helped aligned the company's vision with them. Translated guidelines into static websites with HTML and CSS. </p>
+          <p><span>Guidelines & Banner design</span><span>Photomanipulation</span></p>
+        </VerticalTimelineElement >
+        <VerticalTimelineElement icon={<LaptopMacOutlinedIcon/>}>
+          <h2>GRAPHIC DESIGNER - 11. 2018 - 09. 2022</h2>
+          <p className='experience-city'>Berlin</p>
+          <p>LemonOne GmbH</p>
+          <p>Supported the development of the start-up with visuals for internal use, such as guidelines, logos and printable materials, as well as external such as client guidelines. Managed a team of more than 20 external partners and helped aligned the company's vision with them. Translated guidelines into static websites with HTML and CSS. </p>
+          <p><span>Guidelines & Banner design</span><span>Photomanipulation</span></p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement icon={<LocalLibraryOutlinedIcon/>}>
+        <h2>GRAPHIC DESIGNER - 11. 2018 - 09. 2022</h2>
+          <p className='experience-city'>Berlin</p>
+          <p>LemonOne GmbH</p>
+          <p>Supported the development of the start-up with visuals for internal use, such as guidelines, logos and printable materials, as well as external such as client guidelines. Managed a team of more than 20 external partners and helped aligned the company's vision with them. Translated guidelines into static websites with HTML and CSS. </p>
+          <p><span>Guidelines & Banner design</span><span>Photomanipulation</span></p>
         </VerticalTimelineElement>
         <VerticalTimelineElement>
-          <h1>second element</h1>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement>
-          <h1>third element</h1>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement>
-          <h1>fourth element</h1>
-        </VerticalTimelineElement>
+        <h2>GRAPHIC DESIGNER - 11. 2018 - 09. 2022</h2>
+          <p className='experience-city'>Berlin</p>
+          <p>LemonOne GmbH</p>
+          <p>Supported the development of the start-up with visuals for internal use, such as guidelines, logos and printable materials, as well as external such as client guidelines. Managed a team of more than 20 external partners and helped aligned the company's vision with them. Translated guidelines into static websites with HTML and CSS. </p>
+          <p><span>Guidelines & Banner design</span><span>Photomanipulation</span></p>
+        </VerticalTimelineElement> */}
+        {
+          experienceDb.map(element => {
+            console.log(element)
+            return(
+              <VerticalTimelineElement
+                
+              />
+            )
+          })
+        }
+
       </VerticalTimeline>
       </div>
     </div>
---
+++ src/helpers/experienceDb.js
@@ -0,0 +1,32 @@
+export const experienceDb = [
+  {
+    id: 1,
+    position: 'Web Development Bootcamp',
+    date: '10.2022 - 12.2022',
+    city: 'Berlin',
+    company: "Le Wagon Bootcamp Berlin",
+    description: "xxx",
+    icon: 'education',
+    tags: ['Guidelines & Banner design', 'Photomanipulation']
+  },
+  {
+    id: 2,
+    position: 'Graphic Designer',
+    date: '11.2018 - 10.2022',
+    city: 'Berlin',
+    company: "LemonOne Gmbh",
+    description: "Supported the development of the start-up with visuals for internal use, such as guidelines, logos and printable materials, as well as external such as client guidelines. Managed a team of more than 20 external partners and helped aligned the company's vision with them. Translated guidelines into static websites with HTML and CSS. ",
+    icon: 'work',
+    tags: ['Guidelines & Banner design', 'Photomanipulation', 'Website design']
+  },
+  {
+    id: 3,
+    position: '2D Artist',
+    date: '05.2018 - 08.2018',
+    city: 'Netherlands',
+    company: "World 3D Games",
+    description: "During my time in World 3D Games, I reinforced their art team by creating compelling visuals for their PlayStore games. It ranged from character textures, cover splash art, character art and App icons. ",
+    icon: 'work',
+    tags: ['Character concept art', 'Game icons', 'Model textures']
+  },
+]
