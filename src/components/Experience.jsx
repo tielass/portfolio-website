9e506520217ede1f5ab5ff472f1a927b795776ec
Mihail Mihaylov <tielass@gmail.com>
2023-06-17 19:54:11 +0300

    education DB added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,53 +5,41 @@ import '../style/Experience.css'
 import {experienceDb} from '../helpers/experienceDb'
 import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
 import LaptopMacOutlinedIcon from '@mui/icons-material/LaptopMacOutlined';
-import LocalLibraryOutlinedIcon from '@mui/icons-material/LocalLibraryOutlined';
 
 function Experience() {
+  let workIconStyles = { background: '#FFBA2F'}
+  let schoolIconStyles = { background: "#00D3BA"}
+
   return (
     <div className='experience'>
       <h2 className='experience--header'>Education & Experience</h2>
       <div className="experience--container">
       <VerticalTimeline>
-        {/* <VerticalTimelineElement icon={<SchoolOutlinedIcon className='timeline--icon'/>} >
-          <h2>GRAPHIC DESIGNER - 11. 2018 - 09. 2022</h2>
-          <p className='experience-city'>Berlin</p>
-          <p>LemonOne GmbH</p>
-          <p>Supported the development of the start-up with visuals for internal use, such as guidelines, logos and printable materials, as well as external such as client guidelines. Managed a team of more than 20 external partners and helped aligned the company's vision with them. Translated guidelines into static websites with HTML and CSS. </p>
-          <p><span>Guidelines & Banner design</span><span>Photomanipulation</span></p>
-        </VerticalTimelineElement >
-        <VerticalTimelineElement icon={<LaptopMacOutlinedIcon/>}>
-          <h2>GRAPHIC DESIGNER - 11. 2018 - 09. 2022</h2>
-          <p className='experience-city'>Berlin</p>
-          <p>LemonOne GmbH</p>
-          <p>Supported the development of the start-up with visuals for internal use, such as guidelines, logos and printable materials, as well as external such as client guidelines. Managed a team of more than 20 external partners and helped aligned the company's vision with them. Translated guidelines into static websites with HTML and CSS. </p>
-          <p><span>Guidelines & Banner design</span><span>Photomanipulation</span></p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement icon={<LocalLibraryOutlinedIcon/>}>
-        <h2>GRAPHIC DESIGNER - 11. 2018 - 09. 2022</h2>
-          <p className='experience-city'>Berlin</p>
-          <p>LemonOne GmbH</p>
-          <p>Supported the development of the start-up with visuals for internal use, such as guidelines, logos and printable materials, as well as external such as client guidelines. Managed a team of more than 20 external partners and helped aligned the company's vision with them. Translated guidelines into static websites with HTML and CSS. </p>
-          <p><span>Guidelines & Banner design</span><span>Photomanipulation</span></p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement>
-        <h2>GRAPHIC DESIGNER - 11. 2018 - 09. 2022</h2>
-          <p className='experience-city'>Berlin</p>
-          <p>LemonOne GmbH</p>
-          <p>Supported the development of the start-up with visuals for internal use, such as guidelines, logos and printable materials, as well as external such as client guidelines. Managed a team of more than 20 external partners and helped aligned the company's vision with them. Translated guidelines into static websites with HTML and CSS. </p>
-          <p><span>Guidelines & Banner design</span><span>Photomanipulation</span></p>
-        </VerticalTimelineElement> */}
         {
           experienceDb.map(element => {
-            console.log(element)
+            let isWorkIcon = element.icon === 'work'
             return(
               <VerticalTimelineElement
-                
-              />
+                key={element.id}
+                date={element.date}
+                dateClassName='date'
+                iconStyle={ isWorkIcon  ? workIconStyles : schoolIconStyles }
+                icon = { isWorkIcon ? <LaptopMacOutlinedIcon/> : <SchoolOutlinedIcon/> }
+              >
+              <h2>{element.position}</h2>
+              <p>{element.company} - {element.city}</p>
+              <p>{element.description}</p>
+              <ul className='experience--tags'>
+                {element.tags.map( tag => {
+                  return(
+                    <li className='experience--tag'>{tag}</li>
+                  )
+                })}
+              </ul>
+              </VerticalTimelineElement>
             )
           })
         }
-
       </VerticalTimeline>
       </div>
     </div>
--- src/helpers/experienceDb.js
+++ src/helpers/experienceDb.js
@@ -3,17 +3,17 @@ export const experienceDb = [
     id: 1,
     position: 'Web Development Bootcamp',
     date: '10.2022 - 12.2022',
-    city: 'Berlin',
-    company: "Le Wagon Bootcamp Berlin",
-    description: "xxx",
+    city: 'Berlin, Germany',
+    company: "Le Wagon Bootcamp",
+    description: "9-week full-time intensive coding Bootcamp learning HTML, CSS, Bootstrap,JavaScript ES6, SQL, git, GitHub, Heroku and Ruby on Rails. Designed, implemented and shipped to production a Rails prototype of https://www.giftit.one/",
     icon: 'education',
-    tags: ['Guidelines & Banner design', 'Photomanipulation']
+    tags: []
   },
   {
     id: 2,
     position: 'Graphic Designer',
     date: '11.2018 - 10.2022',
-    city: 'Berlin',
+    city: 'Berlin, Germany',
     company: "LemonOne Gmbh",
     description: "Supported the development of the start-up with visuals for internal use, such as guidelines, logos and printable materials, as well as external such as client guidelines. Managed a team of more than 20 external partners and helped aligned the company's vision with them. Translated guidelines into static websites with HTML and CSS. ",
     icon: 'work',
@@ -21,6 +21,16 @@ export const experienceDb = [
   },
   {
     id: 3,
+    position: 'Programming Basics and Fundamentals',
+    date: '10.2021 - 02.2022',
+    city: 'remote',
+    company: "SoftUni",
+    description: "The module examines fundamental concepts in programming that are the basis for effective and quality learning in the professional modules. Students build on the knowledge gained in the Programming Basics course and begin to use more complex programming techniques and structures such as: arrays, lists, objects and classes.",
+    icon: 'education',
+    tags: ['Data Structures', 'HTML & CSS', 'HTTP Basics', "JavaScript"]
+  },
+  {
+    id: 4,
     position: '2D Artist',
     date: '05.2018 - 08.2018',
     city: 'Netherlands',
@@ -28,5 +38,35 @@ export const experienceDb = [
     description: "During my time in World 3D Games, I reinforced their art team by creating compelling visuals for their PlayStore games. It ranged from character textures, cover splash art, character art and App icons. ",
     icon: 'work',
     tags: ['Character concept art', 'Game icons', 'Model textures']
+  },
+  {
+    id: 5,
+    position: 'Media Art and Design - Master of Fine Arts (M.F.A)',
+    date: '10.2013 - 06.2016',
+    city: 'Weimar, Germany',
+    company: "Bauhaus University Weimar",
+    description: "Creativity, design and experimentation - open to current trends, but also with a view to the future: that is the philosophy of the Bauhaus-Universität Weimar's Master's degree programme in Medienkunst/Mediengestaltung (Media Art and Design). The curriculum covers organisation and technology, as well as developing creative, analytical and critical skills. ",
+    icon: 'education',
+    tags: ['User Interface', 'Game Design', 'Animation', "Typography", "3D Modelling"]
+  },
+  {
+    id: 6,
+    position: 'Graphic Designer',
+    date: '10.2011 - 02.2013',
+    city: 'Sofia, Bulgaria',
+    company: "Mining ltd. Sofia",
+    description: "Developed the company's online presence by creating visuals for their website, product catalogues and promotional materials such as banners and flyers. Strengthened client relationship by working closely on their desired designs.",
+    icon: 'work',
+    tags: ['Catalogue design', 'Corporate vision', 'Print design']
+  },
+  {
+    id: 7,
+    position: 'Engineering Design - Bachelor',
+    date: '09.2005 - 10.2009',
+    city: 'Sofia, Bulgaria',
+    company: "Technical University Sofia",
+    description: "During my time in World 3D Games, I reinforced their art team by creating compelling visuals for their PlayStore games. It ranged from character textures, cover splash art, character art and App icons. ",
+    icon: 'education',
+    tags: ['Drawing analysis', 'Methods of Plasticity', 'History of Art and Design', 'Font and Caligraphy', 'Ergonomics', 'Industrial Design']
   },
 ]
