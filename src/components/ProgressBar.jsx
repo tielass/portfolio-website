a5a369f1a9e2c8974d4ed7b975b79b38576ad80f
Mihail Mihaylov <tielass@gmail.com>
2023-08-15 21:24:00 +0200

    progress bar animation

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -4,34 +4,17 @@ import '../style/ProgressBar.css'
 const ProgressBar = (props) => {
   const { bgcolor, completed, skill } = props;
 
-  const containerStyles = {
-    height: 20,
-    backgroundColor: "#e0e0de",
-    marginTop: 10,
-  }
-
   const fillerStyles = {
-    height: '100%',
     width: `${completed}%`,
     backgroundColor: bgcolor,
-    borderRadius: 'inherit',
-    textAlign: 'right',
-    transition: 'width 1s ease-in-out'
+    transition: 'width 2s ease-in-out'
   }
-
-  const labelStyles = {
-    padding: 5,
-    color: '#000',
-    fontWeight: 'bold'
-  }
-
-
 
   return (
-    <div style={containerStyles}>
-      <div style={fillerStyles} className='progress--bar--container'>
-        <span style={labelStyles}>{skill}</span>
-        <span style={labelStyles}>{`${completed}%`}</span>
+    <div className='progress--bar--wrapper'>
+      <div style={{ ...fillerStyles, '--completed': `${completed}%` }} className='progress--bar--container'>
+        <span className='progress--bar--skill'>{skill}</span>
+        <span className='progress--bar--completed'>{`${completed}%`}</span>
       </div>
     </div>
   );
